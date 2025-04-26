eb3a5abc11b1f1844bdfc090c10d7a6af217d730
aryaman22-hubb <aryamanpanwar187@gmail.com>
2025-04-26 10:45:58 +0530

    Completed

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose")
+
+const app = express();
+app.use(express.json());
+
+
+const userSchema = new mongoose.Schema({
+  Email: { type: String, required: true, unique: true },
+  Password: { type: String, required: true },
+});
+
+const User = mongoose.model("newuser", userSchema);
+
+
+app.post("/login", async (req, res) => {
+  try {
+    const {Email, Password} = req.body;
+
+    if (!Email) {
+      return res.status(400).json({ message: "Email cannot be empty" });
+    }
+
+    if (!Password) {
+      return res.status(400).json({ message: "Password cannot be empty" });
+    }
+
+    const newUser = new User({ Email, Password });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
